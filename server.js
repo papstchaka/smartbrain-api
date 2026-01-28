e74dfaa36a764c5b89147e1e0c7663b66fca887a
papstchaka <alex.christoph.ac@gmail.com>
2026-01-28 10:07:00 +0100

    Give complete update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt-nodejs");
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const knex = require('knex');
 const RateLimit = require('express-rate-limit');
